c8bfeecf8721967e0dfaf4790a53cd4ac5b88094
Deepjyoti <deepjyoti0072@gmail.com>
2025-05-03 22:13:09 +0530

    updated code

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      App
+      <video src="http://localhost:3000/video1"></video>
+      App
+    </div>
+  );
+}
+
+export default App;
--- server/app.js
+++ server/app.js
@@ -1,4 +1,10 @@
 import express from "express";
+import { readFileSync } from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
 
 const PORT = 3000;
 
@@ -6,6 +12,12 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/video", (req, res) => {
+  const file = `${__dirname}/public/video1.mkv`;
+
+  res.sendFile(file);
 });
 
 app.listen(PORT, () => {
